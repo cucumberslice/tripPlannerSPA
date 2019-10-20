04cdd98f469bfa23d915c29cf8937373200ede97
cucmberslice <ywblackberry@gmail.com>
2019-10-20 12:32:19 -0400

    installed mapboxgl

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello from JS')
+const mapboxgl  =require("mapbox-gl")
+
+mapboxgl.accessToken = "your api token here"
+
+const map = new mapboxgl.Map({
+    container: "map",
+    center: [-74.009, 40.705], //fullstack NY coordinnates,
+    zoom: 12, //starting zoom
+    style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+    
+})
